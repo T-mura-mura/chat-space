92ad83ed76cec4eab758396980171554f1972524
T-mura-mura <takumu.kawamura@gmail.com>
2019-11-09 15:12:12 +0900

    unicorn起動せず
    成功時のunicorn.errと見比べると、pathが違うので。消してみた。

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,8 +27,8 @@ set :keep_releases, 5
 
 # S3（画像アップロード用サーバー）を利用するための設定
 set :default_env, {
-  rbenv_root: "/usr/local/rbenv",
-  path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
+#  rbenv_root: "/usr/local/rbenv",
+#  path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
   AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
   AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"]
 }
